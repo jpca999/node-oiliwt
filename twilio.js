0800d712db9bcb841391a8e839e9cb774ec067ea
jpca999 <jpca999@gmail.com>
2021-05-29 16:45:17 -0700

    fist ocmmit

---
+++ twilio.js
@@ -0,0 +1,14 @@
+const phoneNumber= '+19095528238'; 
+const phoneNumberSid= 'AC81db6b6aae46652049527fa2a5f73824'; 
+
+const tokenSid='AC584e5f30a89554f072eed2bacddbe983';
+
+const tokenSecret='4b0784e720bfeffc311b4524923e571e';
+
+
+// acc sid:  AC81db6b6aae46652049527fa2a5f73824
+// Autho token : 9bc24f412ecb2918c449a2c0a0cec46e
+
+
+// var accountSid = 'AC584e5f30a89554f072eed2bacddbe983'; // Your Account SID from www.twilio.com/console
+// var authToken = '4b0784e720bfeffc311b4524923e571e';   // Your Auth Token from www.twilio.com/console
