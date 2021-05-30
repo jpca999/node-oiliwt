6c83ef302001c34cc7eaa7c8c565417244bedf55
jpca999 <jpca999@gmail.com>
2021-05-29 19:30:15 -0700

    twilio class created

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ yarn start
 
 
 
+
+
+    // account - libertytrustgroup
+const phoneNumber= '+19095528238'; 
+const phoneNumberSid= 'AC81db6b6aae46652049527fa2a5f73824'; 
+// from generated api 
+const tokenSid='SK3089b959a87b143da11071b9014c6541';
+const tokenSecret='GWT6SiwPkq7DMEXznbCAX76sr1PyjzUN';
+
+const accountSid='AC81db6b6aae46652049527fa2a5f73824'; 
+const verify = 'VA24a4002c8bdd5a1108d482e7e7a0f408';
+
+
--- twilio.js
+++ twilio.js
@@ -1,9 +1,25 @@
-// account - libertytrustgroup
-const phoneNumber= '+19095528238'; 
-const phoneNumberSid= 'AC81db6b6aae46652049527fa2a5f73824'; 
-// from generated api 
-const tokenSid='SK3089b959a87b143da11071b9014c6541';
-const tokenSecret='GWT6SiwPkq7DMEXznbCAX76sr1PyjzUN';
+const Twilio = require('twilio');
 
-const accountSid='AC81db6b6aae46652049527fa2a5f73824'; 
-const verify = 'VA24a4002c8bdd5a1108d482e7e7a0f408';
+
+class Twilio{
+    phoneNumber= '+19095528238'; 
+    phoneNumberSid= 'AC81db6b6aae46652049527fa2a5f73824'; 
+    tokenSid='SK3089b959a87b143da11071b9014c6541';
+    tokenSecret='GWT6SiwPkq7DMEXznbCAX76sr1PyjzUN';
+    accountSid='AC81db6b6aae46652049527fa2a5f73824'; 
+    verify = 'VA24a4002c8bdd5a1108d482e7e7a0f408';
+    client; 
+    constructor(){
+        this.client = twilio(this.tokenSid, this.tokenSecret,{
+            accountSid: this.accountSid,
+        });
+    }
+        getTwilio(){
+            this.client;
+        }
+}
+const instance = new Twilio()
+
+ Object.freeze(instance);
+
+ module.exports = instance
