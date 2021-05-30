78843beb68f30ec6f6e6393856ab94cefb7d78ac
jpca999 <jpca999@gmail.com>
2021-05-29 17:48:57 -0700

    -

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,14 @@ app.get('/test', (req, res) =>{
     res.send('welcome to express')
 }); 
 
+app.get('/login', (req, res) =>{
+    res.send('logging in')
+}); 
+
+app.get('/verify', (req, res) =>{
+    res.send('verifying')
+}); 
+
 app.listen(PORT, () =>{
     console.log(' port listneing.on 4000 ')
 }); 
